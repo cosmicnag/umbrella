ad4efee115f2f522ea0a78be2cc4d6a4646cc9e9
Sanjay B <b@pad.ma>
2013-02-10 14:00:10 +0530

    Added pyjade and updated requirements.

--- ol/ol/settings.py
+++ ol/ol/settings.py
@@ -111,9 +111,11 @@ except NameError:
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    ('pyjade.ext.django.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+        'django.template.loaders.eggs.Loader',
+    )),
 )
 
 MIDDLEWARE_CLASSES = (
