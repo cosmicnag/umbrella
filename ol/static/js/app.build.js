de5747742ab1a7d4bfe1cb750982d2a2c6534570
Sanjay B <b@pad.ma>
2013-03-31 18:37:50 +0530

    Ran optimizer

---
+++ ol/static/js/app.build.js
@@ -0,0 +1,69 @@
+({
+    baseUrl: ".",
+    paths:{
+        // RequireJS plugin
+        text:'libs/text',
+        // RequireJS plugin
+        domReady:'libs/domReady',
+        // underscore library
+        underscore:'libs/underscore',
+        // Backbone.js library
+        backbone:'libs/backbone',
+        'coffee-script':'libs/coffee-script',
+		backbone_paginator: 'libs/backbone.paginator',
+		marionette: 'libs/backbone.marionette.min',
+		cs: 'libs/cs',
+        tpl: 'libs/tpl',
+        // jQuery
+        jquery:'libs/jquery-1.8.3.min',
+        typeahead: 'libs/typeahead.min'
+    },
+    shim:{
+        backbone:{
+            deps:['underscore', 'jquery'],
+            exports:'Backbone'
+        },
+		backbone_paginator : {
+			deps:['backbone']
+		},
+		marionette: {
+            deps:['backbone'],
+            exports: 'Marionette'	
+		},
+        underscore:{
+            exports:'_'
+        },
+		cs: {
+			deps:['coffee-script']
+		},
+		jquery : {
+			exports:'$'
+		},
+        typeahead: {
+            deps:['jquery'],
+            exports: '$'
+        }
+    },
+    name: "main",
+    out: "build/main.js",
+    include: [
+        "cs!app/init",
+        "cs!app/utils/ajax",
+        "cs!app/core/globals",
+        "cs!app/core/settings",
+        "cs!app/core/mediator",
+        "cs!app/models/book",
+        "cs!app/views/filter",
+        "cs!app/views/bookview",
+        "cs!app/views/menu",
+        "cs!app/views/layouts/content",
+        "cs!app/views/booksview",
+        "cs!app/views/modals/signin",
+        "cs!app/views/modals/borrow",
+        "cs!app/views/modals/signup",
+        "cs!app/collections/books",
+        "cs!app/helpers/user",
+        "cs!app/helpers/modal",
+        "cs!app/helpers/book"
+    ]
+})
